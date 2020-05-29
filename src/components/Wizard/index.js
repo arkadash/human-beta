aae6e93b6c3e5a73d88033f3e5ce9a48daa4d0cf
Moshe Arkadash <moshe.arkadash@fiverr.com>
2020-05-29 10:18:20 +0300

    added default

---
+++ src/components/Wizard/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import StepWizard from 'react-step-wizard';
+
+const Wizard = () => {
+    return (
+        <StepWizard>
+            <div>
+                Step 1
+            </div>
+            <div>
+                Step 2
+            </div>
+        </StepWizard>
+    );
+};
+
+export default Wizard;
