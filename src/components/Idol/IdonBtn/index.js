f76c1cb08b0e524f63075c970a7c041f1bfab37c
Moshe Arkadash <moshe.arkadash@fiverr.com>
2020-06-04 01:45:13 +0300

    created idol

--- src/components/Idol/IdonBtn/index.js
+++ src/components/Idol/IdonBtn/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { noop } from  'lodash';
-import './style.scss';
+import classNames from 'classnames';
+import buttonsMap from './buttonsMap';
+import './styles.scss';
 
-const IdolBtn  = ({ onClick = noop }) => {
+const IdolBtn  = ({ onClick = noop, name = 'KIN_KARDASHIAN', alt = false }) => {
+    const data = buttonsMap[name];
+    const title = data.title;
+    const multipleWords = title.split(' ').length > 1 && title.length > 12;
 
     return (
-        <button onClick={onClick} className="idol-button">
-            button
+        <button onClick={onClick} className={classNames('idol-button', {'idol-button-with-image': data.img})}>
+            {data.img &&
+                <>
+                    <img src={data.img} alt={title}/>
+                    <div className={classNames('idol-image-overlay', {'idol-image-overlay-alt': alt})}/>
+                </>
+            }
+            <div className={classNames('idol-title', {'idol-title-two-words': multipleWords})}>
+                { title }
+            </div>
         </button>
     )
 };
--- src/components/Idol/index.js
+++ src/components/Idol/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { noop } from  'lodash';
 import FolderPageBase from '../common/FolderPageBase';
-import './style.scss';
+import './styles.scss';
+import IdolBtn from "./IdonBtn";
 
 const Idol = ({ nextStep = noop}) => {
 
@@ -11,7 +12,28 @@ const Idol = ({ nextStep = noop}) => {
             nextStep={nextStep}
             header="Who is your idol?">
             <div className="idol-wrapper">
-
+                <div className="buttons-row">
+                    <IdolBtn name="KING_JAMES" alt/>
+                    <IdolBtn name="KIN_KARDASHIAN"/>
+                    <IdolBtn name="BUDDHA" alt/>
+                    <IdolBtn name="JUSTIN_BIEBER"/>
+                    <IdolBtn name="FATHER" alt/>
+                </div>
+                <div className="buttons-row">
+                    <IdolBtn name="MOTHER"/>
+                    <IdolBtn name="ADOLF_HITLER" alt/>
+                    <IdolBtn name="KELLY_JENNER"/>
+                    <IdolBtn name="CRISTIANO" alt/>
+                    <IdolBtn name="DONALD_TRUMP"/>
+                    <IdolBtn name="KENDALL_JENNER" alt/>
+                </div>
+                <div className="buttons-row">
+                    <IdolBtn name="ALBERT_EINSTEIN"/>
+                    <IdolBtn name="THE_ROCK" alt/>
+                    <IdolBtn name="LEO_MESSI"/>
+                    <IdolBtn name="BEYONCE" alt/>
+                    <IdolBtn name="ZUCK" />
+                </div>
             </div>
         </FolderPageBase>
     );
