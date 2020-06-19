cb5085bc5be8dd9dd92d447d0058a3c115b9bb0f
Moshe Arkadash <moshe.arkadash@fiverr.com>
2020-06-19 10:09:22 +0300

    added steal overlay

--- src/components/Steal/index.js
+++ src/components/Steal/index.js
@@ -6,14 +6,20 @@ import './styles.scss';
 
 const Steal = ({...rest}) => {
     const [displaySelection, setDisplaySelection] = useState(false);
-    const [selected, setSelection] = useState('');
+    const [selected, setSelectionState] = useState('');
 
     useEffect(() => {
         setTimeout(() => setDisplaySelection(true), 1500)
     }, [])
 
     const text = '>>“Dont still!”<<';
-
+    const setSelection = (key) => {
+        if(selected === key) {
+            setSelectionState('');
+        } else {
+            setSelectionState(key);
+        }
+    }
 
     return (
         <BaseComponent className="steal" {...rest}>
@@ -21,8 +27,9 @@ const Steal = ({...rest}) => {
                 <div className="steal-title">
                     {text}
                 </div>
-                <div className={classNames('main-image', {'show-image': displaySelection})}>
+                <div className={classNames('main-image', {'show-image': displaySelection, 'selected-back': selected === 'back'})}>
                     <img src={`${ASSETS_BASE}/steal/wallet.png`} alt="sol_e"/>
+                    { selected && <div className="img-selected-overlay"/> }
                 </div>
                 <div className={classNames('steal-buttons-wrapper', {'show-buttons': displaySelection})}>
                     <button className={classNames('steal-button-take', {'btn-selected': selected === 'take'})}
