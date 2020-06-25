218446d8bf66060ef330c26ab1b0da0dfb529572
Moshe Arkadash <moshe.arkadash@fiverr.com>
2020-06-25 10:48:28 +0300

    rest new format

--- src/components/Rest/RestBtn/index.js
+++ src/components/Rest/RestBtn/index.js
@@ -14,11 +14,13 @@ const RestBtn  = ({ onClick = noop, name = 'FACEBOOK', selectedFunc = noop}) => 
             onClick(name);
             setSelection(!selected);
         }} className={classNames('rest-button', {'rest-selected': selected})}>
-            <img src={data.img} alt={title}/>
+            <div className="rest-img-wrapper">
+                <img src={data.img} alt={title} className={`res-img-${name.toLowerCase()}`}/>
+                <div className={classNames('rest-btn-overlay')}/>
+            </div>
             <div className={classNames('rest-btn-title')}>
                 { title }
             </div>
-            <div className={classNames('rest-btn-overlay')}/>
         </button>
     )
 };
