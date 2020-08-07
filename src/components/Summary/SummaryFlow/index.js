6203114903e69912d9a79826f10650984d5be40b
Moshe Arkadash <moshe.arkadash@fiverr.com>
2020-08-07 23:52:54 +0300

    performance demone

--- src/components/Summary/SummaryFlow/index.js
+++ src/components/Summary/SummaryFlow/index.js
@@ -1,19 +1,22 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {ASSETS_BASE} from '../../../constants';
+import classNames from 'classnames';
 import './styles.scss';
 
-export default ({ onChar }) => {
+export default ({ onChar, isAngel }) => {
 
     return (
         <>
-            <div className="summary-statistics-flow">
-                <img src={`${ASSETS_BASE}/summary/cards/starsDouble.png`} alt="sol_e" className="summary-flow-stars"/>
+            <div className={classNames('summary-statistics-flow', {'summary-statistics-flow-demon': !isAngel})}>
+                <img src={isAngel?
+                    `${ASSETS_BASE}/summary/cards/starsDouble.png`: `${ASSETS_BASE}/summary/cards/starsDoubleRed.png`
+                } alt="sol_e" className="summary-flow-stars"/>
                 <div className="summary-statistics-title">
                     Performance.
                 </div>
                 <div className="summary-statistics-data">
                     <div className="summary-data-images">
-                        <img src={`${ASSETS_BASE}/summary/cards/performanceMain.png`}
+                        <img src={isAngel? `${ASSETS_BASE}/summary/cards/performanceMain.png`: `${ASSETS_BASE}/summary/cards/performanceMainRed.png`}
                             alt="" className="character"/>
                         <img src={`${ASSETS_BASE}/summary/left2.svg`}
                             alt="" className="data-btn"
